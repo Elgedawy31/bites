c4f832ee484f019dc5210d10b9baa07eba9375c2
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-08-22 23:45:07 +0300

    make server works

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express  from "express";
+
+const port = process.env.PORT || 3000;
+
+const app  = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}).on('error' , (error) =>{
+  throw new Error(error.message || "Server is not running");
+});
